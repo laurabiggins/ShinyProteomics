03d387f8dafc5903c0170a41dbe5ff66d6b9a9c6
Laura Biggins <bigginsl@babraham.ac.uk>
2019-09-05 11:39:31 +0100

    minor change

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -12,7 +12,7 @@ golem::add_module( name = "my_other_module" ) # Name of the module
 
 ## 2.2 Add dependencies
 
-usethis::use_package( "thinkr" ) # To call each time you need a new package
+#usethis::use_package( "thinkr" ) # To call each time you need a new package
 usethis::use_package( "ggplot2" )
 usethis::use_package( "magrittr" )
 usethis::use_package( "dplyr" )
